f6d8ff1056243a5c8e23123050235063bf703d11
hussainsalman22 <hussainqureshi22jan@gmail.com>
2023-03-26 15:35:42 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./header";
+import Slider from "./slider";
+let Mainapp = ()=>{
+  return(
+    <>
+   <Header/>
+   <Slider/>
+   </>
+
+  )
+    
+
+}
+
+export default Mainapp
---
+++ src/slider.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Sliderimg from "./assets/banner-1.jpg"
+
+let Slider = ()=>{
+    return(
+        <img src = {Sliderimg}/>
+    )
+}
+export default Slider
